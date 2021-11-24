95fb47b49d967339270c45dedfd793e6ebc7bb19
Farrux Ergashaliev <92575804+Farrux6969@users.noreply.github.com>
2021-11-24 13:13:09 +0500

    Update admin.py

--- app1/admin.py
+++ app1/admin.py
@@ -2,10 +2,6 @@ from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 from .models import *
 
-@admin.register(Soz)
-class SozAdmin(ModelAdmin):
-    search_fields = ('soz',)
-
 admin.site.register(Soz)
 admin.site.register(N_soz)
 
